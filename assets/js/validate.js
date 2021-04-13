22cf92e39ddf37531f90e801ced966dbd355ecc7
td2-front-end-g5 <82113293+td2-front-end-g5@users.noreply.github.com>
2021-04-13 16:07:44 -0400

    Revert "Desarrollo"

--- assets/js/validate.js
+++
@@ -1,9 +0,0 @@
-import { signIn } from '../js/auth.js'
-
-export const validateSigned = () => {
-    //const email = document.getElementById('login-username')
-    //const password = document.getElementById('login-password')
-    const email= 'user@prueba.cl';
-    const password = 'user1234';
-    signIn(email, password);
-}
